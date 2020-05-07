00c67383c9a974db10668a738b03c9b4326f8a30
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-05-07 02:24:34 -0300

    Adding items to cart feature & Reactotron config

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,17 @@
 import { createStore } from 'redux';
 
 import rootReducer from './modules/rootReducer';
-/** O Store nunca pode inicializar vazio, porque se não a aplicação
+/** O store nunca pode inicializar vazio, porque se não a aplicação
  * quebra. No caso nosso carrinho é o reducer, então eu inicializo ele
  * e passo como parâmetro na store. Renomeei o cart para reducer.
  * Mas como eu posso ter vários reducers, temos o combineReducers para
  * combinar todos os reducers em um só lugar e depois unir eles na store
  */
 
-const store = createStore(rootReducer);
+// configurando o Reactotron
+const enhancer =
+  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
